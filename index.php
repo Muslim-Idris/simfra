0444e3100b89c46e163c21cab5b381a83132f36b
Muslim Idris <gelamu@gmail.com>
2012-12-09 19:04:25 +0100

    Renamed all classes to Upper-Case

--- index.php
+++ index.php
@@ -23,13 +23,10 @@
  */
 
 require_once(__DIR__ . '/stdinc.php');
-#append_include_path(__DIR__);
 
-#$objConfig = new Config('main');
 $objConfig = Singleton::getInstance('Config', array('main'));
 
-#$tpl = new Template('index');
-$tpl = Singleton::getInstance('Template', array('index'));
-$tpl->assign('name', 'Muslim');
-$tpl->exec();
+$objTemplate = Singleton::getInstance('Template', array('index'));
+$objTemplate->assign('name', 'Muslim');
+$objTemplate->exec();
 
